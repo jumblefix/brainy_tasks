33f7ba7787919090f4717a9914890afd2b170741
Dhayanandhan <dhayaec@gmail.com>
2019-09-21 18:26:34 +0530

    add connectivity check

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:brainy_tasks/constants/AppConstants.dart';
 import 'package:brainy_tasks/constants/RoutePaths.dart';
+import 'package:brainy_tasks/features/about/AboutViewModel.dart';
+import 'package:brainy_tasks/features/common/ConnectivityService.dart';
+import 'package:brainy_tasks/features/home/HomeViewModel.dart';
 import 'package:brainy_tasks/features/navigation/NavigationService.dart';
 import 'package:brainy_tasks/locator.dart';
 import 'package:brainy_tasks/router.dart';
 import 'package:brainy_tasks/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
@@ -25,12 +29,26 @@ void main() {
 class BrainyTasks extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: AppConstants.app_name,
-      theme: appTheme,
-      initialRoute: RoutePaths.Home,
-      onGenerateRoute: Router.generateRoute,
-      navigatorKey: locator<NavigationService>().navigatorKey,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<HomeViewModel>(
+          builder: (context) => HomeViewModel(),
+        ),
+        ChangeNotifierProvider(
+          builder: (context) => AboutViewModel(),
+        ),
+        StreamProvider<ConnectivityStatus>(
+          builder: (context) =>
+              ConnectivityService().connectionStatusController.stream,
+        )
+      ],
+      child: MaterialApp(
+        title: AppConstants.app_name,
+        theme: appTheme,
+        initialRoute: RoutePaths.Home,
+        onGenerateRoute: Router.generateRoute,
+        navigatorKey: locator<NavigationService>().navigatorKey,
+      ),
     );
   }
 }
